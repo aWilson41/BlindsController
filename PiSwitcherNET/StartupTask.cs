3d406a46f020cf1afcca4048284b7cf158b9404d
Andrew Wilson <andx_roo@live.com>
2018-03-18 14:44:10 -0500

    format

--- PiSwitcherNET/StartupTask.cs
+++ PiSwitcherNET/StartupTask.cs
@@ -7,41 +7,41 @@ using Windows.Devices;
 
 namespace PiSwitcherNET
 {
-    public sealed class StartupTask : IBackgroundTask
-    {
-        BackgroundTaskDeferral deferral;
-        PwmPin pin;
-        double currPercent = 0.01;
-        double prevPercent = 0.01;
+	public sealed class StartupTask : IBackgroundTask
+	{
+		BackgroundTaskDeferral deferral;
+		PwmPin pin;
+		double currPercent = 0.01;
+		double prevPercent = 0.01;
 		bool flip = false;
 
-        // Called when program starts
-        public void Run(IBackgroundTaskInstance taskInstance)
-        {
-            deferral = taskInstance.GetDeferral();
+		// Called when program starts
+		public void Run(IBackgroundTaskInstance taskInstance)
+		{
+			deferral = taskInstance.GetDeferral();
 
 			// Provide callback for messages
 			StaticListener.MessageReceived += ConnectionReceived;
 
-            SetupPWM().Wait();
+			SetupPWM().Wait();
 
-            while (true)
-            {
-                // Wait 60s
-                Task.Delay(60000).Wait();
+			while (true)
+			{
+				// Wait 60s
+				Task.Delay(60000).Wait();
 
 				currPercent = DetermineActiveDutyCycle();
 
-                // If the percentage has changed from the last update
-                if (currPercent != prevPercent)
+				// If the percentage has changed from the last update
+				if (currPercent != prevPercent)
 					RotateServo();
-            }
-        }
+			}
+		}
 
 		// Interpret client messages
 		private async void ConnectionReceived(object sender, SocketEventArgs e)
 		{
-			if (e.message == "A7xrev02")
+			if (e.message == "Andrew")
 			{
 				flip = !flip;
 				currPercent = DetermineActiveDutyCycle();
@@ -50,25 +50,25 @@ namespace PiSwitcherNET
 		}
 
 		private async Task SetupPWM()
-        {
-            if (LightningProvider.IsLightningEnabled)
-            {
-                LowLevelDevicesController.DefaultProvider = LightningProvider.GetAggregateProvider();
-                var pwmControllers = await PwmController.GetControllersAsync(LightningPwmProvider.GetPwmProvider());
-                var pwmController = pwmControllers[1]; // use the on-device controller
-                pwmController.SetDesiredFrequency(50); // try to match 50Hz
-                pin = pwmController.OpenPin(21);
-            }
-        }
+		{
+			if (LightningProvider.IsLightningEnabled)
+			{
+				LowLevelDevicesController.DefaultProvider = LightningProvider.GetAggregateProvider();
+				var pwmControllers = await PwmController.GetControllersAsync(LightningPwmProvider.GetPwmProvider());
+				var pwmController = pwmControllers[1]; // use the on-device controller
+				pwmController.SetDesiredFrequency(50); // try to match 50Hz
+				pin = pwmController.OpenPin(21);
+			}
+		}
 
 		public double DetermineActiveDutyCycle()
 		{
 			// Test what percetange it should be at given the time
 			int hr = DateTime.Now.Hour;
 			int min = DateTime.Now.Minute;
+			// Make some hour decisions
 			if (!flip)
 			{
-				// If hour is over 20 (8pm) or if hour is under 10pm
 				if (hr >= 20 || hr < 10)
 					return 0.025; // Blinds down
 				else
@@ -76,7 +76,6 @@ namespace PiSwitcherNET
 			}
 			else
 			{
-				// If hour 20 (8pm) and past 30 mins (8:30pm) or if past hour is over 20 (8pm) or if hour is under 10
 				if (hr >= 20 || hr < 10)
 					return 0.075; // Blinds up
 				else
@@ -97,5 +96,5 @@ namespace PiSwitcherNET
 			// Stop the servo
 			pin.Stop();
 		}
-    }
+	}
 }
